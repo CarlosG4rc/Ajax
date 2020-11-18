d48b4eae314d845670abdf71a75ed80052827093
Carlos García <67180099+CarlosG4rc@users.noreply.github.com>
2020-11-18 09:00:59 -0600

    Add files via upload

---
+++ js/05.js
@@ -0,0 +1,23 @@
+// pasos para la creación de un request
+
+var btnCargar = document.getElementById('cargar');
+
+function cargarContenido() {
+   // crearlo
+   var xhr = new XMLHttpRequest();
+   // abrirlo
+   xhr.open("GET", "texto_prueba.txt", true);
+   
+   // revisar que cambie
+   xhr.onreadystatechange = function() {
+      if(xhr.readyState == 4 && xhr.status == 200) {
+         var contenido = document.getElementById('contenido');
+         contenido.innerHTML = xhr.responseText;
+      }
+   }
+   xhr.send();
+}
+
+btnCargar.addEventListener('click', cargarContenido );
+
+
